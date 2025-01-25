b86f625433b72c239772f4df0153f41714466a28
NotGiggs <giggslohjayhon2002@gmail.com>
2025-01-25 19:44:11 +0800

    Change Duke to Bartholomew

---
+++ src/main/java/Bartholomew.java
@@ -0,0 +1,8 @@
+public class Bartholomew {
+    public static void main(String[] args) {
+        String name = "Bartholomew";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
