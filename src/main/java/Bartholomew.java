c8cb3f5483e722ba3d4dd7cd11a2039632c0ade3
NotGiggs <giggslohjayhon2002@gmail.com>
2025-02-04 18:48:03 +0800

    Add scanning function to Bartholomew to repeat user inputs

--- src/main/java/Bartholomew.java
+++ src/main/java/Bartholomew.java
@@ -1,8 +1,21 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Bartholomew {
     public static void main(String[] args) {
         String name = "Bartholomew";
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println("Fine day we're having! I am " + name);
+        System.out.println("What may I do for you?");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Goodbye Master! Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+        scanner.close();
     }
 }
